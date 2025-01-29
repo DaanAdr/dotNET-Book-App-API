98fe17a14a48c25aa4451e881df6d1eb05e45dd6
DaanAdr <parsa1815@outlook.com>
2025-01-29 14:34:51 +0100

    Removed example stuff and created a GET endpoint for Genres

---
+++ Book App API/Controllers/GenreController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Book_App_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GenreController : Controller
+    {
+        [HttpGet(Name = "GetGenre")]
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
