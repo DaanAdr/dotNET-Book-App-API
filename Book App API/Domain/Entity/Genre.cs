77a52ec244a1ba7f46119bb4decacee60cd25a9d
DaanAdr <parsa1815@outlook.com>
2025-01-29 13:39:48 +0100

    Added migration file for Genre entity

--- Book App API/Domain/Entity/Genre.cs
+++ Book App API/Domain/Entity/Genre.cs
@@ -1,7 +1,10 @@
-﻿namespace Book_App_API.Domain.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_App_API.Domain.Entity
 {
     public class Genre
     {
+        [Key]
         public Guid Id { get; set; }
         public string GenreName { get; set; }
     }
