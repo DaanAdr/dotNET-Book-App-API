8f0d1a31c1a365a6bd6b67f5275d1597b94e50db
DaanAdr <parsa1815@outlook.com>
2025-01-30 15:03:59 +0100

    Added get endpoint and functions

---
+++ Book App API/Controllers/ReaderAgeController.cs
@@ -0,0 +1,34 @@
+﻿using Book_App_API.Domain.Entity;
+using Book_App_API.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_App_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReaderAgeController : Controller
+    {
+        private readonly ReaderAgeLogic _logic;
+
+        public ReaderAgeController(ReaderAgeLogic logic)
+        {
+            _logic = logic;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<ReaderAge>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                List<ReaderAge> response = await _logic.GetReaderAges();
+                return Ok(response);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
---
+++ Book App API/Infrastructure/Database/DatabaseLogic/ReaderAgeDatabaseLogic.cs
@@ -0,0 +1,20 @@
+﻿using Book_App_API.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_App_API.Infrastructure.Database.DatabaseLogic
+{
+    public class ReaderAgeDatabaseLogic
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ReaderAgeDatabaseLogic(AppDbContext appDbContext)
+        {
+            _dbContext = appDbContext;
+        }
+
+        public async Task<List<ReaderAge>> GetReaderAges()
+        {
+            return await _dbContext.ReaderAges.AsNoTracking().ToListAsync();
+        }
+    }
+}
