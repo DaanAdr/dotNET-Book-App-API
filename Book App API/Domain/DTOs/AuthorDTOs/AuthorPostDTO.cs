5e9ec72971663c9a947196a9b8c07ed969c71172
DaanAdr <parsa1815@outlook.com>
2025-02-23 15:50:06 +0100

    Added test cases for incorrect input for author POST

--- Book App API/Domain/DTOs/AuthorDTOs/AuthorPostDTO.cs
+++ Book App API/Domain/DTOs/AuthorDTOs/AuthorPostDTO.cs
@@ -8,7 +8,7 @@ namespace Book_App_API.Domain.DTOs.AuthorDTOs
         [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Firstname can only contain letters.")]
         public string Firstname { get; set; }
 
-        [Required(ErrorMessage = "Firstname is required.")]
+        [Required(ErrorMessage = "Surname is required.")]
         [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Surname can only contain letters.")]
         public string Surname { get; set; }
     }
